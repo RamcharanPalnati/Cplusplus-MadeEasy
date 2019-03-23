3c4c15964ac981e0596055537d54ab11574ed861
Ram Charan Palnati <Ramcharan.Palnati@vistarait.com>
2019-03-23 11:21:52 +0530

    initial code base

---
+++ HelloWorldC++.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main(){
+    std::cout<<"Hello World Cout\n";
+    return 0;
+}
---
+++ HelloWorldPrintf.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+int main(){
+    printf("%s","Hello World Printf\n");
+    return 0;
+
+}
